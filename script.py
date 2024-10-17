7b51cb715aeb8ec9cba9d83a36493b4a59cd89ef
Jonathan Griffe <jonathan.griffe@gitguardian.com>
2024-10-17 16:17:33 +0200

    secret

--- script.py
+++ script.py
@@ -1,1 +1,1 @@
-password = "alehfiaefoaeijtt'1351351"
+openai_key = "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"
